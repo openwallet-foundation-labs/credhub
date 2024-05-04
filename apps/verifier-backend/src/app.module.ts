3ddddd4e59e414875fd2d151571325f843fe9c39
Mirko Mollik <mollik@trustcerts.de>
2024-05-04 21:34:42 +0200

    fix: migrate verifier to nestjs
    Signed-off-by: Mirko Mollik <mollik@trustcerts.de>

---
+++ apps/verifier-backend/src/app.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+import { VerifierModule } from './verifier/verifier.module';
+
+@Module({
+  imports: [
+    AuthModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        PORT: Joi.number().default(3000),
+        CONFIG_RELOAD: Joi.boolean().default(false),
+        VERIFIER_BASE_URL: Joi.string().required(),
+        NODE_ENVIRONMENT: Joi.string()
+          .valid('development', 'production')
+          .default('development'),
+      }),
+    }),
+    VerifierModule,
+  ],
+  controllers: [AppController],
+})
+export class AppModule {}
---
+++ apps/verifier-backend/src/verifier/verifier.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { RelyingPartyManagerService } from './relying-party-manager.service';
+import { VerifierController } from './verifier.controller';
+import { KeyModule } from 'src/key/key.module';
+
+@Module({
+  imports: [KeyModule],
+  controllers: [VerifierController],
+  providers: [RelyingPartyManagerService],
+})
+export class VerifierModule {}
