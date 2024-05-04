360ed9dba478f2a06078e843773714185021b04f
Mirko Mollik <mollik@trustcerts.de>
2024-05-04 12:57:27 +0200

    Migrate issuer to nestjs Fixes #25
    Signed-off-by: Mirko Mollik <mollik@trustcerts.de>

--- apps/issuer-demo/src/main.ts
+++ apps/issuer-demo/src/main.ts
@@ -1,3 +1,5 @@
+import { authenticateWithKeycloak } from './auth';
+import { config } from './config';
 import './style.css';
 import qrcode from 'qrcode';
 
@@ -7,58 +9,57 @@ interface RequestLinkBody {
   pin?: boolean;
 }
 
-interface Config {
-  issuerUrl: string;
-  credentialId: string;
-}
-
-let config: Config;
-
-//fetch the config
-fetch('/config.json')
-  .then((res) => res.json())
-  .then((res) => {
-    config = res;
-  });
-
 let loop: NodeJS.Timeout;
 
-function getCode() {
+/**
+ * Get the qr code, display it and start the loop to get the status
+ */
+async function getCode() {
+  //TODO: this code will not check if the session is expired and the qr code is not valid anymore
   if (loop) {
     clearInterval(loop);
   }
   const body: RequestLinkBody = {
     credentialId: config.credentialId,
   };
+  const accessToken = await authenticateWithKeycloak();
   fetch(`${config.issuerUrl}/request`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
+      Authorization: `Bearer ${accessToken}`,
     },
     body: JSON.stringify(body),
   })
     .then((res) => res.json())
     .then((res) => {
       //display the qrocde
-      qrcode
-        .toDataURL(res.uri)
-        .then((url) =>
-          (document.getElementById('qr') as HTMLElement).setAttribute(
-            'src',
-            url
-          )
-        );
+      qrcode.toDataURL(res.uri).then((url) => {
+        (document.getElementById('qr') as HTMLElement).setAttribute('src', url);
+        urlInput.value = res.uri;
+      });
       getStatus(res.session.preAuthorizedCode);
       //request for the status update in the background with a loop
       loop = setInterval(() => getStatus(res.session.preAuthorizedCode), 2000);
     });
 }
 
-function getStatus(id: string) {
-  fetch(`${config.issuerUrl}/sessions/${id}`)
+/**
+ * Get the status of the session
+ * @param id
+ */
+async function getStatus(id: string) {
+  const accessToken = await authenticateWithKeycloak();
+  fetch(`${config.issuerUrl}/sessions/${id}`, {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  })
     .then((res) => res.json())
     .then((res) => {
-      console.log(res);
+      if (res.status === 'CREDENTIAL_ISSUED') {
+        clearInterval(loop);
+      }
       status.innerHTML = res.status;
     });
 }
@@ -66,9 +67,11 @@ function getStatus(id: string) {
 (document.querySelector<HTMLDivElement>('#app') as HTMLDivElement).innerHTML = `
   <button id="get">Get Code</button>
   <img id="qr" />
+  <input type="text" id="url" />
   <div id="status"></div>
 `;
 const status = document.getElementById('status') as HTMLDivElement;
+const urlInput = document.getElementById('url') as HTMLInputElement;
 (document.getElementById('get') as HTMLElement).addEventListener(
   'click',
   getCode
