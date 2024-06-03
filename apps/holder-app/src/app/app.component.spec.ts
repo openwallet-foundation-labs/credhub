de61c928ce974bf142fc6f42e1479a0803107b86
Mustafa Alsalfiti <alsalfiti@xignsys.com>
2024-06-03 14:32:40 +0200

    fix: holder-app test
    Signed-off-by: Mustafa Alsalfiti <alsalfiti@xignsys.com>

--- apps/holder-app/src/app/app.component.spec.ts
+++ apps/holder-app/src/app/app.component.spec.ts
@@ -4,11 +4,14 @@ import { AppComponent } from './app.component';
 import { CheckForUpdatesService } from './check-for-updates.service';
 import { SwUpdate } from '@angular/service-worker';
 import { of } from 'rxjs';
+import { SettingsService } from '@my-wallet/holder-shared';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('AppComponent', () => {
   let component: AppComponent;
   let fixture: ComponentFixture<AppComponent>;
   let checkForUpdatesService: CheckForUpdatesService;
+  let settingsService: SettingsService;
 
   beforeAll(() => {
     Object.defineProperty(window, 'matchMedia', {
@@ -24,7 +27,7 @@ describe('AppComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule, AppComponent],
+      imports: [RouterTestingModule, AppComponent, HttpClientModule],
       providers: [
         CheckForUpdatesService,
         { provide: SwUpdate, useValue: { available: of() } }, // Mock SwUpdate
