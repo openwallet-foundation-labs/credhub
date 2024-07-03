5b4f71ddc1b901680b8a7647149d9e97855dad52
Mirko Mollik <mirkomollik@gmail.com>
2024-07-03 11:33:58 +0200

    ci
    Signed-off-by: Mirko Mollik <mirkomollik@gmail.com>

--- apps/holder-app-e2e/playwright.config.ts
+++ apps/holder-app-e2e/playwright.config.ts
@@ -18,6 +18,7 @@ const baseURL = process.env['BASE_URL'] || 'http://localhost:4200';
 export default defineConfig({
   ...nxE2EPreset(__filename, { testDir: './src' }),
   workers: 1,
+  retries: process.env['CI'] ? 2 : 0,
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     baseURL,
