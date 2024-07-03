e2d1191861e4ff1e1773a2fb5fa8fc97e5a4b49e
Mirko Mollik <mirkomollik@gmail.com>
2024-07-03 13:45:58 +0200

    install package
    Signed-off-by: Mirko Mollik <mirkomollik@gmail.com>

--- apps/holder-app/src/environments/environment.ts
+++ apps/holder-app/src/environments/environment.ts
@@ -18,4 +18,7 @@ export const environment = {
   get keycloakClient() {
     return globalThis.environment?.keycloakClient;
   },
+
+  sentryDsn:
+    'https://6350a5821ef72c29668bd6ca1697b108@o4507537672830976.ingest.de.sentry.io/4507537674993744',
 };
